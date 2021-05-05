72e614e70f4334efe5e16a2f95754b3976ea31ad
maniac-tech <ajj_2396@yahoo.com>
2021-05-06 00:46:41 +0530

    -Added new, create and destroy def to sessions_controller -Added new to dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :logged_in_user, only: [:new]
+
+  def new
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      Rails.logger.info "Auth Successful"
+      log_in user
+      redirect_to '/dashboard'
+    else
+      Rails.logger.info "Auth Failed"
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
