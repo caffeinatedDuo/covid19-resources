e3cfef1d42f052cbd0bd02f758007ed3bfbe6060
maniac-tech <ajj_2396@yahoo.com>
2021-05-07 00:15:23 +0530

    Save other telemedics info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,13 @@ class DashboardController < ApplicationController
   def addTelemedic
     Rails.logger.info("addTelemedic")
     doctors_name = params["doctors-name"]
-    Rails.logger.info(doctors_name)
-    new_telemedics_record = Telemedic.create(name: doctors_name)
+    contact_no = params["contact-no"]
+    timings = params["timing"] 
+    verification_status = params["verification-status"]
+    functional_status = params["functional-status"]
+    location = params["location"]
+
+    new_telemedics_record = Telemedic.create(name: doctors_name, contact: contact_no, timings: timings, verified: verification_status, functional: functional_status, location: location)
     new_telemedics_record.save
     Rails.logger.info("doctors_name")
   end
