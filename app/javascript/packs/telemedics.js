d99dcffd24cba4641cb7f3ee1d42ee1fe76c8eb6
maniac-tech <ajj_2396@yahoo.com>
2021-05-07 01:25:00 +0530

    Save the user input to DB

--- app/javascript/packs/telemedics.js
+++ app/javascript/packs/telemedics.js
@@ -1,11 +1,31 @@
+var $addTelemedicsDoctorsName;
+var $addTelemedicsContactNo;
+var $addTelemedicstiming;
+var $addTelemedicsVerificationStatus;
+var $addTelemedicsFunctionalStatus;
+var $addTelemedicsLocation;
+
+function telemedicsInit(){
+  uiBindings();
+}
+
+function uiBindings(){
+  $addTelemedicsDoctorsName = $('#doctors-name');
+  $addTelemedicsContactNo = $('#contact-no');
+  $addTelemedicstiming = $('#timing');
+  $addTelemedicsVerificationStatus = $('#verification-status');
+  $addTelemedicsFunctionalStatus = $('#functional-status');
+  $addTelemedicsLocation = $('#location');
+}
+
 function handleAddTelemedicsFormSubmit(){
   var formData = new FormData();
-  formData.append('doctors-name',"a");
-  formData.append('contact-no',"a");
-  formData.append('timing',"a");
-  formData.append('verification-status',"a");
-  formData.append('functional-status',"a");
-  formData.append('location',"a");
+  formData.append('doctors-name',$addTelemedicsDoctorsName[0].value);
+  formData.append('contact-no', $addTelemedicsContactNo[0].value);
+  formData.append('timing', $addTelemedicstiming[0].value);
+  formData.append('verification-status', $addTelemedicsVerificationStatus[0].value);
+  formData.append('functional-status', $addTelemedicsFunctionalStatus[0].value);
+  formData.append('location', $addTelemedicsLocation[0].value);
 
   $.ajax({
     beforeSend: function (xhr) { xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content')) },
@@ -21,3 +41,5 @@ $('#add-telemedics-form').on('submit', function(e){
   e.preventDefault();
   handleAddTelemedicsFormSubmit();
 });
+
+telemedicsInit();
